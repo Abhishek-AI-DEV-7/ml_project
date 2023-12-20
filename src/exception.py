522f4b788a410da690b0260b4990c3d60d3b1d8d
Abhishek-Dev-Ai <abhishekgaddam09@gmail.com>
2023-12-20 07:38:13 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -18,10 +18,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__=="__main__":
-
-    try:
-        a=1/10
-    except Exception as e:
-        logging.info("divided by zero")
-        raise CustomException(e,sys)
